598f00ca3c68c7f41e0cc715e91dfdb9d41311cd
Saymon <kroxotylik@gmail.com>
2020-04-18 18:15:25 +0300

    Приукрасил меню

--- app/src/main/java/com/example/lmbase/MainActivity.java
+++ app/src/main/java/com/example/lmbase/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.lmbase;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.google.android.material.internal.NavigationMenu;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NavigationMenu menu;
     private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +24,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav);
+        View navView = navigationView.inflateHeaderView(R.layout.heade_nav);
+        mToolbar = findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Домашняя страница");
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                UserMenuSElector(item);
+                return false;
+            }
+        });
+    }
+
+    private void UserMenuSElector(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_home:
+                Toast.makeText(this, "Домой", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_tor_list:
+                Toast.makeText(this, "Список торговых", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_static:
+                Toast.makeText(this, "Статистика", Toast.LENGTH_LONG).show();
+                break;
+
+            case R.id.nav_logout:
+                Toast.makeText(this, "Выйти", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 }
