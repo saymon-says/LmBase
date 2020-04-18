3c5ee6747155e12913e740b229837e944eb18b86
Saymon <kroxotylik@gmail.com>
2020-04-18 17:24:55 +0300

    Добавил меню

--- app/src/main/java/com/example/lmbase/MainActivity.java
+++ app/src/main/java/com/example/lmbase/MainActivity.java
@@ -1,14 +1,22 @@
 package com.example.lmbase;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.internal.NavigationMenu;
+
 public class MainActivity extends AppCompatActivity {
+
+    private NavigationMenu menu;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 }
